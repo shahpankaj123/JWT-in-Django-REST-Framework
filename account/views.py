79aa89ee562d1132f125e42ad3d907def2a78a14
Pankaj Shah <aaryanshah615@gmail.com>
2023-09-21 22:03:50 +0545

    2nd phase

--- account/views.py
+++ account/views.py
@@ -2,7 +2,22 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 # Create your views here.
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Add custom claims
+        token['username'] = user.username
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class=MyTokenObtainPairSerializer
+
+
 @api_view(['GET'])
 def home(request):
     return Response({'msg':'hello'})
